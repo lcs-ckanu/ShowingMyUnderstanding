559f7d695a3f88489f7ad9aa0c16e5cbfbc25359
Chinedum Kanu <ckanu@lcs.on.ca>
2017-11-24 22:24:01 -0500

    Finished Assignment.

--- ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
@@ -24,11 +24,26 @@ let canvas = Canvas(width: 300, height: 300)
 //Bacckground
 let number = random(from: 0, toButNotIncluding: 2)
 if number == 0{
-    canvas.drawRectangle(centreX: 300, centreY: 300, width: 300, height: 40)
+    canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
 } else if number == 1{
-    canvas.drawEllipse(centreX: 150, centreY: 150, width: 40, height: 40)
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: 275, height: 275)
 }
 
+// Draw lolipop
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 40, height: 40)
+//Draw lolipop line
+canvas.lineColor=Color.white
+canvas.drawLine(fromX: 150, fromY: 150, toX: 150, toY: 0)
+
+canvas.translate(byX: 150, byY: 150)
+canvas.drawAxes()
+// Draw lines
+
+for x in stride(from: 150, through: 300, by: 50){
+canvas.drawLine(fromX: 150, fromY: 150, toX: 150, toY: 300)
+    canvas.rotate(by: 90)
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
